f4e436ba56c24f28c4f2c29f9d98b8c718387822
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-04-19 17:26:29 -0600

    create relations of Users, Products and Cart model

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,5 +1,13 @@
+const Cart = require("./cart.model");
+const Products = require("./products.model");
+const Users = require("./users.model");
+
 const initModels = () => {
-  return;
+  Users.hasMany(Cart);
+  Cart.belongsTo(Users);
+
+  Cart.belongsToMany(Products, { through: "CartProducts" });
+  Products.belongsToMany(Cart, { through: "CartProducts" });
 };
 
 module.exports = initModels;
