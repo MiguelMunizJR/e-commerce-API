035c25b9a6c81252609be204d5f5a2add9c71dbf
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-04-22 00:14:50 -0600

    create the orders model

---
+++ src/models/orders.model.js
@@ -0,0 +1,35 @@
+const db = require("../db/database");
+const Users = require("./users.model");
+const Datatypes = require("sequelize").DataTypes;
+
+const Orders = db.define("orders", {
+  id: {
+    type: Datatypes.UUID,
+    allowNull: false,
+    primaryKey: true
+  },
+  total: {
+    type: Datatypes.INTEGER,
+    allowNull: false
+  },
+  userId: {
+    type: Datatypes.UUID,
+    allowNull: false,
+    field: "user_id",
+    references: {
+      key: "id",
+      model: Users
+    }
+  },
+  status: {
+    type: Datatypes.ENUM("pending", "completed"),
+    allowNull: false,
+    defaultValue: "pending"
+  },
+  date: {
+    type: Datatypes.DATEONLY,
+    allowNull: false
+  }
+});
+
+module.exports = Orders;
