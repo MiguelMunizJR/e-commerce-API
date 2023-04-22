174c592efaf30799ac93e247fcd4b607e806fd28
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-04-22 02:05:04 -0600

    add relations (pending order products relation)

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,5 +1,7 @@
 const Cart = require("./cart.model");
 const CartProducts = require("./cart_products.model");
+const Orders = require("./orders.model");
+const OrderProduct = require("./orders_products.model");
 const Products = require("./products.model");
 
 const initModels = () => {
@@ -8,6 +10,15 @@ const initModels = () => {
 
   Cart.belongsToMany(Products,{ through: CartProducts });
   Products.belongsToMany(Cart, { through: CartProducts });
+
+  Orders.hasMany(Products);
+  Products.belongsTo(Orders);
+
+  Orders.hasMany(OrderProduct);
+  OrderProduct.belongsTo(Orders);
+
+  Products.hasMany(OrderProduct);
+  OrderProduct.belongsTo(Products);
 };
 
 module.exports = initModels;
