77101d92906fb76e6a134c013749ca428eff250d
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-04-25 17:20:55 -0600

    add 10 smartphones in db products

--- src/utils/seeders/products.js
+++ src/utils/seeders/products.js
@@ -171,4 +171,89 @@ const productsLaptops = [
   },
 ];
 
-Products.bulkCreate(productsLaptops);
+// Products.bulkCreate(productsLaptops);
+
+const productsSmartphones = [
+  {
+    id: UUID.v4(),
+    title: "Xiaomi Redmi Note 11 6GB RAM 128GB ROM Azul",
+    price: 6200,
+    description: "Sumérgete en un mundo de maravillas. La pantalla retroiluminada ofrece lo último en brillo, contraste, calibración del color y resolución. Este procesador no solo es rápido y potente, sino que además funciona con el proceso de 6 nm de bajo consumo para ofrecer un alto rendimiento a la vez que mejora la duración de la batería. Carga hasta el 100 % en aproximadamente una hora con la carga rápida Pro de 33 W* del Redmi Note 11, gracias a la nueva tecnología MMT de Redmi Note.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/V6tcvyvy/Smartphone-Xiaomi-Redmi-Note-11-6-GB-RAM-128-GB-ROM-Azul.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "iPhone 11 Pro Max 64GB Plata",
+    price: 11000,
+    description: "Apple iPhone 11 Pro Max 64 GB color plata; el iPhone Pro tiene un revolucionario sistema de tres cámaras, muy fácil de usar, que multiplica tu capacidad creativa. Y trae un chip sorprendente que potencia el aprendizaje automático y redefine lo que un smartphone puede hacer. Además, la duración de la batería mejoró como nunca. Te presentamos un iPhone tan poderoso que tuvimos que llamarlo Pro.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/PJzsbm12/Smartphone-i-Phone-11-PRO-MAX-64-GB-Plata.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Samsung Galaxy S10 128GB Negro",
+    price: 9100,
+    description: "Una pantalla infinita cinemática inmersiva, una cámara profesional y PowerShare inalámbrico. La próxima generación está aquí. La identificación ultrasónica de huellas dactilares en pantalla protege y desbloquea con el primer toque. La cámara de nivel profesional captura sin esfuerzo imágenes épicas y de calidad profesional del mundo tal como lo ves. Accede de forma inteligente a la energía aprendiendo cómo y cuándo usas tu teléfono.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/CKz0ynVx/Samsung-Galaxy-S10-128-GB-Negro.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Samsung Galaxy S21 128Gb Rosa",
+    price: 12589,
+    description: "Mira tus series y películas favoritas con la mejor definición a través de su pantalla Dynamic AMOLED 2X de 6.2&#168;. Disfruta de colores brillantes y detalles precisos en todos tus contenidos. Descubre infinitas posibilidades para tus fotos con las 3 cámaras principales de tu equipo. Pon a prueba tu creatividad y juega con la iluminación, diferentes planos y efectos para obtener grandes resultados. ¡Desenchúfate! Con la súper batería de 4000 mAh tendrás energía por mucho más tiempo para jugar, ver series o trabajar sin necesidad de realizar recargas.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/8zyGnB9Y/Samsung-Galaxy-S21-128-Gb-Rosa.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Motorola Moto G82 5G 128GB 6GB Gris",
+    price: 6299,
+    description: "Prepárate para la próxima generación de redes con un smartphone fabricado para el mañana. Gracias a la potencia del procesador Qualcomm Snapdragon 695 y los 6 GB de RAM, tendrás la velocidad que necesitas para conectarte a redes 5G superrápidas. Con un desempeño hasta 30 % más rápido, juega sin interrupciones, reproduce videos sin problemas con 5G y toma fotos profesionales. A nadie le gusta estresarse por tener batería baja, especialmente antes de una salida nocturna importante o de un largo día en el trabajo. Con TurboPower 30, obtén horas de uso en solo minutos de carga y libérate de preocupaciones.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/kGdPtjR2/Motorola-Moto-G82-5-G-128-GB-6-GB-Gris.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Xiaomi 13 5G 256GB 8GB Negro",
+    price: 16800,
+    description: "El nuevo celular de Xiaomi tiene resistencia al agua y al polvo, pues ofrece una certificación IP68, ya que tiene fabricación de precisión, así podrás llevar a este chiquito a cualquier parte y grabar en todos lados sin miedo, ya está listo para todo tipo de aventuras. Tus fotos saldrán de lujo con el Xiaomi 13, ya que cuenta con un sistema de cámara profesional Leica para que captures grandes detalles, la principal es de 50 MP, la segunda es una cámara teleobjetivo de 10 MP y la última es ultra gran angular Leica de 12 MP, para que todo salga como lo imaginas.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/PfZtrDvL/Xiaomi-13-5-G-256-GB-8-GB-Negro.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Honor 50 Lite RAM 128GB Negro",
+    price: 5420,
+    description: "El Honor 50 Lite es un gama media con servicios de Google. Su capa de personalización sigue siendo Magic UI 4.2, prácticamente un calco de EMUI, la ROM de Huawei, todo ello corriendo bajo Android 11. El procesador es el Qualcomm Snapdragon 662 (procesador 4G), acompañado de dos generosas configuraciones de RAM: 6. La memoria interna es de 128 GB y todo ello está alimentado por una batería de 4.300mAh con carga rápida de 66W, una cifra sobresaliente y más propia de un gama alta.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/15QygzDv/Honor-50-Lite-RAM-128-GB-Negro.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Huawei Nova Y70 128 GB Blanco",
+    price: 6399,
+    description: "Con Huawei nova Y70 haz frente a cualquier reto con la confianza de tener energía para todo el día. Su destacada batería de 6000 mAh, con tecnología Huawei SuperCharge de 22.5W, aporta horas de entretenimiento con pocos minutos de carga para ese maratón de series en su pantalla FullView de 6.75 pulgadas con ajuste inteligente de luz. Obtén imágenes definidas y brillantes, ya sea en tomas distantes o en acercamientos, con su sistema de triple cámara con IA de 48  5  2 megapixeles. Con su cámara selfie de 8 megapixeles consigue retratos suaves que resalten tus atributos en cada toma.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/L84RT0Cf/Huawei-Nova-Y70-128-GB-Blanco.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "iPhone 14 Pro Apple Negro Espacial 256GB",
+    price: 28500,
+    description: "Lo mejor mejora aún con la Pro Perfection del iPhone 14 Pro. Es potente, tiene cámaras increíbles, tiene una hermosa pantalla, y Dynamic Island es un buen reemplazo de muesca. Desde su factor de forma de bolsillo hasta sus caballos de fuerza y capacidades de cámara, el iPhone 14 Pro aplasta cualquier otra cosa que intente mantenerse en su contra. Simplemente tan cerca de un teléfono perfecto jamás visto.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/FsVN0QZN/i-Phone-14-Pro-Apple-Negro-Espacial-256-GB.webp"
+  },
+  {
+    id: UUID.v4(),
+    title: "Motorola Edge 30 Pro 12GB 256GB Verde",
+    price: 11200,
+    description: "Con el smartphone Edge 30 Pro podrás capturar tanto fotos como videos de alta calidad gracias a sus dos cámaras de 50 MP, por lo mismo, podrás enfocar tu objetivo rápidamente y con gran precisión. Y ni se diga cómo quedarán tus videos ya que permite una calidad 8K con colores precisos. El Moto 30 Pro ofrece una pantalla Max Vision con tecnología OLED, por lo que podrás estar muy segur@ de que tus series, videos y más tendrán los mejores colores, así como negros profundos para cualquier contenido. Y ni se diga con su tasa de refresco de 144 Hz, es tan rápida como tú haciendo todo a última hora.",
+    category: "Smartphones",
+    image: "https://i.postimg.cc/brQ8CXf0/Motorola-Edge-30-Pro-12-GB-256-GB-Verde.webp"
+  },
+];
+
+Products.bulkCreate(productsSmartphones);
